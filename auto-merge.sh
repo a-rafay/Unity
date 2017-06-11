dd1683357ae40e008b52e77a103a77dbbb5c3d6f
Abdur Rafay <abdurrafay@fico.com>
2017-06-10 19:34:46 -0700

    Adding auto merge script to auto merge to master branch.

---
+++ auto-merge.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Automatically merge the last commit through the following branches:
+# Paranoid -} master
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LAST_COMMIT=$(git rev-list -1 HEAD)
+
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to master
+
+case $CURRENT_BRANCH in
+Paranoid)
+  git checkout master ＆＆ git merge Paranoid
+  git checkout $CURRENT_BRANCH
+  ;;
+esac
